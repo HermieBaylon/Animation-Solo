6a0857105fd7753238721b6117ddba0aa06ca8ec
Hermie Baylon <hbaylonb@gmail.com>
2021-01-20 00:14:49 -0800

    small edits

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// var gameEngine = new GameEngine();
-// var ASSET_MANAGER = new AssetManager();
-
-// ASSET_MANAGER.downloadAll(function () {
-// 	var canvas = document.getElementById('gameWorld');
-// 	var ctx = canvas.getContext('2d');
-
-// 	gameEngine.init(ctx);
-// 	gameEngine.start();
-// });
 
 var gameEngine = new GameEngine();
 var ASSET_MANAGER = new AssetManager();
