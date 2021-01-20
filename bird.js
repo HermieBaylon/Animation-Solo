a47d6b11231a510584e105d58c16a898ee9dcf11
Hermie Baylon <hbaylonb@gmail.com>
2021-01-20 00:32:51 -0800

    Add a third bird

--- bird.js
+++ bird.js
@@ -8,16 +8,21 @@ class Bird {
 
         this.x2 = 200;
         this.y2 = 60;
+
+        this.x3 = 400;
+        this.y3 = 100;
         
         this.game = game;
         this.animate1 = new Animator(this.spritesheet, 0, 95, 115, 115, 4, 0.15, 0, false, true);
-        this.animate2 = new Animator(this.spritesheet, 8, 432, 115, 115, 4, 0.15, 0, false, true);
+        this.animate2 = new Animator(this.spritesheet, 8, 432, 115, 115, 4, 0.2, 0, false, true);
+        this.animate3 = new Animator(this.spritesheet, 0, 95, 115, 115, 4, 0.15, 0, false, true);
        
     }
 
     draw(ctx) {
         this.animate1.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1);
         this.animate2.drawFrame(this.game.clockTick, ctx, this.x2, this.y2, 1);
+        this.animate3.drawFrame(this.game.clockTick, ctx, this.x3, this.y3, 1);
     }
 
     update() {
